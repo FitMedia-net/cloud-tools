f9dc802b923d80780dc49ef41b454c75ee5bee98
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-07-26 15:49:34 +0100

    Update super-sticky-params.js

--- super-sticky-params.js
+++ super-sticky-params.js
@@ -1,5 +1,5 @@
 // Analytics Script
-defer data-domain="attribution.fitmedia.cloud" src="https://plausible.io/js/script.js"
+defer data-domain="cdn.fitmedia.cloud" src="https://plausible.io/js/script.js"
 
 // Catch and append all URL parameters to all hyperlinks that contain one of the below domains
 (function() {
