fab99b99d09c490f4681c8e2f2652b877ef2a881
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-07-26 17:19:05 +0100

    Update super-sticky-params.js

--- super-sticky-params.js
+++ super-sticky-params.js
@@ -1,45 +1,35 @@
-// Catch and append all URL parameters to all hyperlinks that contain one of the below domains
 (function() {
-  var domainsToDecorate = [
-          'fitmedia.net'
-      ],
-      queryParams = [
-          'utm_medium', 
-          'utm_source',
-          'utm_campaign',
-          'fm_campaign',
-          'fm_adgroup',
-          'fm_ad',
-          'ref'
-      ]
-  // do not edit anything below this line
-  var links = document.querySelectorAll('a'); 
+  var domainsToDecorate = ['fitmedia.net'];
+  var queryParams = ['utm_medium', 'utm_source', 'utm_campaign', 'fm_campaign', 'fm_adgroup', 'fm_ad', 'ref'];
 
-// check if links contain domain from the domainsToDecorate array and then decorates
-  for (var linkIndex = 0; linkIndex < links.length; linkIndex++) {
-      for (var domainIndex = 0; domainIndex < domainsToDecorate.length; domainIndex++) { 
-          if (links[linkIndex].href.indexOf(domainsToDecorate[domainIndex]) > -1 && links[linkIndex].href.indexOf("#") === -1) {
-              links[linkIndex].href = decorateUrl(links[linkIndex].href);
-          }
-      }
+  var links = document.querySelectorAll('a');
+
+  links.forEach(function(link) {
+    if (shouldDecorate(link)) {
+      link.href = decorateUrl(link.href);
+    }
+  });
+
+  function shouldDecorate(link) {
+    return domainsToDecorate.some(function(domain) {
+      return link.href.includes(domain) && !link.href.includes("#");
+    });
   }
-// decorates the URL with query params
+
   function decorateUrl(urlToDecorate) {
-      urlToDecorate = (urlToDecorate.indexOf('?') === -1) ? urlToDecorate + '?' : urlToDecorate + '&';
-      var collectedQueryParams = [];
-      for (var queryIndex = 0; queryIndex < queryParams.length; queryIndex++) {
-          if (getQueryParam(queryParams[queryIndex])) {
-              collectedQueryParams.push(queryParams[queryIndex] + '=' + getQueryParam(queryParams[queryIndex]))
-          }
-      }
-      return urlToDecorate + collectedQueryParams.join('&');
+    var collectedQueryParams = queryParams.filter(function(param) {
+      return getQueryParam(param);
+    }).map(function(param) {
+      return param + '=' + getQueryParam(param);
+    });
+
+    return urlToDecorate + (collectedQueryParams.length ? '?' + collectedQueryParams.join('&') : '');
   }
 
-  // borrowed from https://stackoverflow.com/questions/831030/
-  // a function that retrieves the value of a query parameter
   function getQueryParam(name) {
-      if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(window.location.search))
-          return decodeURIComponent(name[1]);
+    var regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)');
+    var match = regex.exec(window.location.search);
+    return match ? decodeURIComponent(match[1]) : null;
   }
 
 })();
