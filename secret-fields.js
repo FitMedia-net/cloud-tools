f4e19f9ad6513c3a9430510a64d439d4f13ca2a3
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-07-31 16:44:46 +0100

    Update secret-fields.js

--- secret-fields.js
+++ secret-fields.js
@@ -1,42 +1,30 @@
-<script>
-  window.onload = function() {
-        // Get all forms on the page
-        var forms = document.getElementsByTagName('form');
-  
-        // Loop through each form
-        for(var i = 0; i < forms.length; i++) {
-          
-          // Create a new hidden input fields
-          var inputMedium = document.createElement('url_medium');
-          var inputCampaign = document.createElement('url_campaign');
-          var inputSource = document.createElement('url_source');
-          var inputRef = document.createElement('url_ref');
-          
-          // Set the input type to hidden
-          inputMedium.setAttribute('type', 'hidden');
-          inputCampaign.setAttribute('type', 'hidden');
-          inputSource.setAttribute('type', 'hidden');
-          inputRef.setAttribute('type', 'hidden');
-          
-          // Set the name attributes
-          inputMedium.setAttribute('name', 'hiddenField');
-          inputCampaign.setAttribute('name', 'hiddenField');
-          inputSource.setAttribute('name', 'hiddenField');
-          inputRef.setAttribute('name', 'hiddenField');
-  
-          
-          // Set the value attributes
-          inputMedium.setAttribute('value', 'hiddenValue');
-          inputCampaign.setAttribute('value', 'hiddenValue');
-          inputSource.setAttribute('value', 'hiddenValue');
-          inputRef.setAttribute('value', 'hiddenValue');
-  
-  
-          // Append the hidden input to the form
-          forms[i].appendChild(inputMedium);
-          forms[i].appendChild(inputCampaign);
-          forms[i].appendChild(inputSource);
-          forms[i].appendChild(inputRef);
-        }
-      }
-</script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Function to get all URL parameters
+            function getAllUrlParams() {
+                var params = {};
+                var search = location.search.substring(1);
+
+                if (search) {
+                    search.split('&').forEach(function(param) {
+                        var item = param.split('=');
+                        params[item[0]] = item[1] && decodeURIComponent(item[1].replace(/\+/g, ' '));
+                    });
+                }
+
+                return params;
+            };
+
+            // Create hidden fields for each URL parameter
+            var params = getAllUrlParams();
+            Object.keys(params).forEach(function(key) {
+                var input = document.createElement("input");
+
+                input.setAttribute("type", "hidden");
+                input.setAttribute("name", key);
+                input.setAttribute("id", key);
+                input.setAttribute("value", params[key]);
+
+                // append to form
+                document.getElementById("myForm").appendChild(input);
+            });
+        });
