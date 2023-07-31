ccac0600f9c618504eadb5cc993edd1626d7201d
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-07-31 16:31:39 +0100

    Update sticky_params.js

--- sticky_params.js
+++ sticky_params.js
@@ -1,1 +1,33 @@
-<script src="https://cdn.jsdelivr.net/gh/FitMedia-net/cloud-tools@main/super-sticky-params.js"></script>
+(function() {
+  var currentDomain = window.location.hostname;
+  var queryParams = ['utm_medium', 'utm_source', 'utm_campaign', 'fm_campaign', 'fm_adgroup', 'fm_ad', 'ref'];
+
+  var links = document.querySelectorAll('a');
+
+  links.forEach(function(link) {
+    if (shouldDecorate(link)) {
+      link.href = decorateUrl(link.href);
+    }
+  });
+
+  function shouldDecorate(link) {
+    return link.hostname === currentDomain && !link.href.includes("#");
+  }
+
+  function decorateUrl(urlToDecorate) {
+    var collectedQueryParams = queryParams.filter(function(param) {
+      return getQueryParam(param);
+    }).map(function(param) {
+      return param + '=' + getQueryParam(param);
+    });
+
+    return urlToDecorate + (collectedQueryParams.length ? '?' + collectedQueryParams.join('&') : '');
+  }
+
+  function getQueryParam(name) {
+    var regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)');
+    var match = regex.exec(window.location.search);
+    return match ? decodeURIComponent(match[1]) : null;
+  }
+
+})();
