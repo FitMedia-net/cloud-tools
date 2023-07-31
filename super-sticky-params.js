cfab98aeef874f2b6daa294dc586e6c98945ea1b
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-07-31 17:16:37 +0100

    Update super-sticky-params.js

--- super-sticky-params.js
+++ super-sticky-params.js
@@ -1,35 +1,66 @@
-(function() {
-  var domainsToDecorate = ['fitmedia.net'];
-  var queryParams = ['utm_medium', 'utm_source', 'utm_campaign', 'fm_campaign', 'fm_adgroup', 'fm_ad', 'ref'];
+document.addEventListener('DOMContentLoaded', function() {
+    var currentDomain = window.location.hostname;
+    var queryParams = ['utm_medium', 'utm_source', 'utm_campaign', 'fm_campaign', 'fm_adgroup', 'fm_ad', 'ref'];
 
-  var links = document.querySelectorAll('a');
+    function getAllUrlParams() {
+        var params = {};
+        var search = location.search.substring(1);
 
-  links.forEach(function(link) {
-    if (shouldDecorate(link)) {
-      link.href = decorateUrl(link.href);
+        if (search) {
+            search.split('&').forEach(function(param) {
+                var item = param.split('=');
+                params[item[0]] = item[1] && decodeURIComponent(item[1].replace(/\+/g, ' '));
+            });
+        }
+
+        return params;
+    };
+
+    // Create hidden fields for each URL parameter
+    var params = getAllUrlParams();
+    var forms = document.getElementsByTagName('form');
+    
+    Array.from(forms).forEach(function(form) {
+        Object.keys(params).forEach(function(key) {
+            var input = document.createElement("input");
+
+            input.setAttribute("type", "hidden");
+            input.setAttribute("name", key);
+            input.setAttribute("id", key + "-" + form.id); // adjusted id to avoid duplications
+            input.setAttribute("value", params[key]);
+
+            // append to form
+            form.appendChild(input);
+        });
+    });
+
+    var links = document.querySelectorAll('a');
+
+    links.forEach(function(link) {
+        if (shouldDecorate(link)) {
+            link.href = decorateUrl(link.href);
+        }
+    });
+
+    function shouldDecorate(link) {
+        // Consider sub-domains by checking if the link's hostname ends with the current domain
+        return (link.hostname === currentDomain || link.hostname.endsWith('.' + currentDomain)) && !link.href.includes("#");
     }
-  });
 
-  function shouldDecorate(link) {
-    return domainsToDecorate.some(function(domain) {
-      return link.href.includes(domain) && !link.href.includes("#");
-    });
-  }
+    function decorateUrl(urlToDecorate) {
+        var collectedQueryParams = queryParams.filter(function(param) {
+            return getQueryParam(param);
+        }).map(function(param) {
+            return param + '=' + getQueryParam(param);
+        });
 
-  function decorateUrl(urlToDecorate) {
-    var collectedQueryParams = queryParams.filter(function(param) {
-      return getQueryParam(param);
-    }).map(function(param) {
-      return param + '=' + getQueryParam(param);
-    });
+        return urlToDecorate + (collectedQueryParams.length ? '?' + collectedQueryParams.join('&') : '');
+    }
 
-    return urlToDecorate + (collectedQueryParams.length ? '?' + collectedQueryParams.join('&') : '');
-  }
+    function getQueryParam(name) {
+        var regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)');
+        var match = regex.exec(window.location.search);
+        return match ? decodeURIComponent(match[1]) : null;
+    }
 
-  function getQueryParam(name) {
-    var regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)');
-    var match = regex.exec(window.location.search);
-    return match ? decodeURIComponent(match[1]) : null;
-  }
-
-})();
+});
