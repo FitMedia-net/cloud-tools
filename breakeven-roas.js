8a47a8fb3928b39c566badbdeeabafc2c08bfd71
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-07-31 10:29:21 +0100

    Update breakeven-roas.js

--- breakeven-roas.js
+++ breakeven-roas.js
@@ -1,16 +1,13 @@
-///////////////////////////////////////////////
-// BREAK EVEN - ROAS
-///////////////////////////////////////////////
-const roasForm = document.querySelector('[dt-element="roas-form"]');
-const inputAdSpend = document.querySelector('[dt-element="ad-spend"]');
-const inputRevenue = document.querySelector('[dt-element="rev"]');
-const inputAvgSaleValue = document.querySelector('[dt-element="avg-sale-value"]');
-const inputCogs = document.querySelector('[dt-element="cogs"]');
-const inputRoasMail = document.querySelector('[dt-element="roas-mail"]');
-const inputRoasCheckbox = document.querySelector('[dt-element="roas-toc"]');
-const btnRoasSubmit = document.querySelector('[dt-element="roas-submit"]');
-const labelRoasValue = document.querySelector('[dt-element="roas-value"]');
-const labelTargetRoasValue = document.querySelector('[dt-element="target-roas-value"]');
+const roasForm = document.querySelector('[element="roas-form"]');
+const inputAdSpend = document.querySelector('[element="ad-spend"]');
+const inputRevenue = document.querySelector('[element="rev"]');
+const inputAvgSaleValue = document.querySelector('[element="avg-sale-value"]');
+const inputCogs = document.querySelector('[element="cogs"]');
+const inputRoasMail = document.querySelector('[element="roas-mail"]');
+const inputRoasCheckbox = document.querySelector('[element="roas-toc"]');
+const btnRoasSubmit = document.querySelector('[element="roas-submit"]');
+const labelRoasValue = document.querySelector('[element="roas-value"]');
+const labelTargetRoasValue = document.querySelector('[element="target-roas-value"]');
 
 function isValidEmail(email) {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})$/;
