22660688773a16d33dc90498917a17092acc6286
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-07-31 17:14:13 +0100

    Update secret-fields.js

--- secret-fields.js
+++ secret-fields.js
@@ -1,30 +1,34 @@
-        document.addEventListener('DOMContentLoaded', function() {
-            // Function to get all URL parameters
-            function getAllUrlParams() {
-                var params = {};
-                var search = location.search.substring(1);
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to get all URL parameters
+    function getAllUrlParams() {
+        var params = {};
+        var search = location.search.substring(1);
 
-                if (search) {
-                    search.split('&').forEach(function(param) {
-                        var item = param.split('=');
-                        params[item[0]] = item[1] && decodeURIComponent(item[1].replace(/\+/g, ' '));
-                    });
-                }
-
-                return params;
-            };
-
-            // Create hidden fields for each URL parameter
-            var params = getAllUrlParams();
-            Object.keys(params).forEach(function(key) {
-                var input = document.createElement("input");
-
-                input.setAttribute("type", "hidden");
-                input.setAttribute("name", key);
-                input.setAttribute("id", key);
-                input.setAttribute("value", params[key]);
-
-                // append to form
-                document.getElementById("myForm").appendChild(input);
+        if (search) {
+            search.split('&').forEach(function(param) {
+                var item = param.split('=');
+                params[item[0]] = item[1] && decodeURIComponent(item[1].replace(/\+/g, ' '));
             });
+        }
+
+        return params;
+    };
+
+    // Create hidden fields for each URL parameter
+    var params = getAllUrlParams();
+    var forms = document.getElementsByTagName('form');
+    
+    Array.from(forms).forEach(function(form) {
+        Object.keys(params).forEach(function(key) {
+            var input = document.createElement("input");
+
+            input.setAttribute("type", "hidden");
+            input.setAttribute("name", key);
+            input.setAttribute("id", key + "-" + form.id); // adjusted id to avoid duplications
+            input.setAttribute("value", params[key]);
+
+            // append to form
+            form.appendChild(input);
         });
+    });
+});
