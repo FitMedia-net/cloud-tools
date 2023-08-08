c5f39335882d0496206c74b3980b72961e6ef0d9
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-08-08 11:05:32 +0100

    Update sticky-params.js

--- sticky-params.js
+++ sticky-params.js
@@ -1,35 +1,45 @@
-// Last updated on 2nd August 2023
-(function() {
-  var currentDomain = window.location.hostname;
-  var queryParams = ['fm_campaign', 'fm_adgroup', 'fm_ad', fm_keyword', 'ref'];
+// Last updated on 8th August 2023
+document.addEventListener("DOMContentLoaded", function() {
 
-  var links = document.querySelectorAll('a');
+  (function() {
+    var currentDomain = window.location.hostname;
+    var queryParams = ['fm_campaign', 'fm_adgroup', 'fm_ad', 'fm_keyword', 'ref'];
 
-  links.forEach(function(link) {
-    if (shouldDecorate(link)) {
-      link.href = decorateUrl(link.href);
-    }
-  });
+    var links = document.querySelectorAll('a');
 
-  function shouldDecorate(link) {
-    // Consider sub-domains by checking if the link's hostname ends with the current domain
-    return (link.hostname === currentDomain || link.hostname.endsWith('.' + currentDomain)) && !link.href.includes("#");
-  }
-
-  function decorateUrl(urlToDecorate) {
-    var collectedQueryParams = queryParams.filter(function(param) {
-      return getQueryParam(param);
-    }).map(function(param) {
-      return param + '=' + getQueryParam(param);
+    links.forEach(function(link) {
+      if (shouldDecorate(link)) {
+        link.href = decorateUrl(link.href);
+      }
     });
 
-    return urlToDecorate + (collectedQueryParams.length ? '?' + collectedQueryParams.join('&') : '');
-  }
+    function shouldDecorate(link) {
+      // Consider sub-domains by checking if the link's hostname ends with the current domain
+      var isSubDomain = link.hostname === currentDomain || 
+                        (link.hostname.endsWith('.' + currentDomain) && link.hostname.split('.').slice(-2).join('.') === currentDomain);
 
-  function getQueryParam(name) {
-    var regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)');
-    var match = regex.exec(window.location.search);
-    return match ? decodeURIComponent(match[1]) : null;
-  }
+      return isSubDomain && !link.href.includes("#");
+    }
 
-})();
+    function decorateUrl(urlToDecorate) {
+      var collectedQueryParams = queryParams.filter(function(param) {
+        return getQueryParam(param);
+      }).map(function(param) {
+        return param + '=' + getQueryParam(param);
+      });
+
+      // Check if the URL already has query parameters
+      var separator = urlToDecorate.includes('?') ? '&' : '?';
+      return urlToDecorate + (collectedQueryParams.length ? separator + collectedQueryParams.join('&') : '');
+    }
+
+    function getQueryParam(name) {
+      var regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)');
+      var match = regex.exec(window.location.search);
+      return match ? decodeURIComponent(match[1]) : null;
+    }
+
+  })();
+
+});
+
