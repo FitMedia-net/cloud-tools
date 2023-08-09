9fdfb1e27e079f7d6db4c74f812529211440b0c4
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-08-09 09:05:34 +0100

    Update sticky-params.js

--- sticky-params.js
+++ sticky-params.js
@@ -1,65 +1,52 @@
-// Last updated on 8th August 2023
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOMContentLoaded event fired!");
+// Last updated on 9th August 2023
+(function() {
+    // Get current URL
+    const currentUrl = new URL(window.location.href);
+    
+    console.log("Current URL:", currentUrl.href);
 
-    (function() {
-        var currentDomain = window.location.hostname;
-        console.log("Current domain:", currentDomain);
-        var queryParams = ['fm_campaign', 'fm_adgroup', 'fm_ad', 'fm_keyword', 'ref'];
+    // List of parameters to search for
+    const searchParamsList = ['fm_campaign', 'fm_adgroup', 'fm_ad', 'fm_keyword', 'ref'];
 
-        var links = document.querySelectorAll('a');
-        console.log("Number of links found:", links.length);
+    // Collect the values of the listed parameters if they exist
+    const existingParams = {};
+    searchParamsList.forEach(param => {
+        const value = currentUrl.searchParams.get(param);
+        if (value) {
+            existingParams[param] = value;
+        }
+    });
 
-        links.forEach(function(link, index) {
-            console.log("Inspecting link", index + 1, "with href:", link.href);
-            if (shouldDecorate(link)) {
-                console.log("Decorating link", index + 1);
-                link.href = decorateUrl(link.href);
-                console.log("New href for link", index + 1, ":", link.href);
-            } else {
-                console.log("Not decorating link", index + 1);
+    console.log("Existing Parameters:", existingParams);
+
+    if (Object.keys(existingParams).length === 0) {
+        console.log("No existing parameters found from the list.");
+        return;
+    }
+
+    // Search through all the hyperlinks on the page
+    const hyperlinks = document.querySelectorAll("a");
+
+    hyperlinks.forEach(link => {
+        try {
+            const linkUrl = new URL(link.href);
+
+            // Check if hyperlink shares the same domain as the current page
+            if (linkUrl.hostname === currentUrl.hostname) {
+                console.log("Processing hyperlink:", link.href);
+                
+                // Append the parameters from existingParams to the hyperlink
+                for (const [key, value] of Object.entries(existingParams)) {
+                    linkUrl.searchParams.set(key, value);
+                }
+                
+                link.href = linkUrl.href;
+                console.log("Updated hyperlink:", link.href);
             }
-        });
-
-        function shouldDecorate(link) {
-            // Consider sub-domains by checking if the link's hostname ends with the current domain
-            var isSubDomain = link.hostname === currentDomain || 
-                            (link.hostname.endsWith('.' + currentDomain) && link.hostname.split('.').slice(-2).join('.') === currentDomain);
-            
-            var shouldDecorateResult = isSubDomain && !link.href.includes("#");
-            console.log("shouldDecorate result for", link.href, ":", shouldDecorateResult);
-
-            return shouldDecorateResult;
+        } catch (e) {
+            console.warn("Invalid hyperlink found or error processing:", link.href);
         }
+    });
 
-        function decorateUrl(urlToDecorate) {
-            var collectedQueryParams = queryParams.filter(function(param) {
-                return getQueryParam(param);
-            }).map(function(param) {
-                return param + '=' + getQueryParam(param);
-            });
-
-            console.log("Collected query params:", collectedQueryParams);
-
-            // Check if the URL already has query parameters
-            var separator = urlToDecorate.includes('?') ? '&' : '?';
-            var decoratedUrl = urlToDecorate + (collectedQueryParams.length ? separator + collectedQueryParams.join('&') : '');
-
-            console.log("Decorated URL:", decoratedUrl);
-            return decoratedUrl;
-        }
-
-        function getQueryParam(name) {
-            var regex = new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)');
-            var match = regex.exec(window.location.search);
-            var paramValue = match ? decodeURIComponent(match[1]) : null;
-            
-            console.log("Query param", name, "value:", paramValue);
-
-            return paramValue;
-        }
-
-    })();
-
-});
-
+    console.log("Processing complete!");
+})();
