93be766f09da0d2c327ccd6b929a5fec9b6e1306
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2023-12-12 11:27:42 +0000

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,33 +1,37 @@
 (function() {
-    // Function to get the GTM ID from the query string
-    function getQueryParam(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, '\\$&');
-        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    // Function to get the GTM ID from the query string of the script's own URL
+    function getQueryParam(name) {
+        var scriptTags = document.getElementsByTagName('script');
+        for (var i = 0; i < scriptTags.length; i++) {
+            if (scriptTags[i].src.indexOf('tracking.js') > -1) {
+                var url = scriptTags[i].src;
+                name = name.replace(/[\[\]]/g, '\\$&');
+                var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                    results = regex.exec(url);
+                if (!results || !results[2]) return null;
+                return decodeURIComponent(results[2].replace(/\+/g, ' '));
+            }
+        }
+        return null;
     }
 
-    var id = getQueryParam('id'); // Get Tracking ID from URL parameter
+    var id = getQueryParam('id'); // Get Tracking ID from the script's URL parameter
     if (!id) {
         console.error('Tracking ID not found in URL');
-        console.log('Script URL:', url);
         return;
     }
 
     // Inject the <head> script
     var headScript = document.createElement('script');
     headScript.async = true;
-    headScript.textContent = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://tracking.fitmedia.cloud/zpdfwzls.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${id}');`;
+    headScript.textContent = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://tracking.fitmedia.cloud/zpdfwzls.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-${id}');`;
     document.head.appendChild(headScript);
 
     // Inject the <body> script (no-script tag)
     window.addEventListener('DOMContentLoaded', (event) => {
         var noscript = document.createElement('noscript');
         var iframe = document.createElement('iframe');
-        iframe.src = `https://tracking.fitmedia.cloud/ns.html?id=${id}`;
+        iframe.src = `https://tracking.fitmedia.cloud/ns.html?id=GTM-${id}`;
         iframe.height = '0';
         iframe.width = '0';
         iframe.style.display = 'none';
@@ -37,3 +41,4 @@
         document.body.insertBefore(noscript, document.body.firstChild);
     });
 })();
+
